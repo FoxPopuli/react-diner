6cfa5753de10efb0da4143e6145d8437506ef47c
Taylor Casse <taylorcasse@gmail.com>
2023-09-17 14:51:20 +0200

    Add greeter animations

--- src/components/Greeter/Greeter.js
+++ src/components/Greeter/Greeter.js
@@ -1,15 +1,36 @@
 import classes from "./Greeter.module.css";
 import DinerButton from "../DinerButton/DinerButton";
-
+import { useEffect, useState } from "react";
 const Greeter = () => {
   const clickHanlder = () => {
     console.log("Clicked!");
   };
+
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   console.log("Hi");
+    //   setIsFirstLoad(false);
+    // }, 50);
+    setIsFirstLoad(false);
+  }, []);
+
   return (
-    <div className={classes.greeterMain}>
-      <h1>Flavor, Precision and Perfection</h1>
-      <h2>We are Jane Doe's Diner and we serve food and drinks</h2>
-      <div className={classes.buttonContainer}>
+    <div
+      className={`${classes.greeterMain} ${!isFirstLoad ? classes.loaded : ""}`}
+    >
+      <h1 className={`${classes.item1} ${!isFirstLoad ? classes.loaded : ""}`}>
+        Flavor, Precision and Perfection
+      </h1>
+      <h2 className={`${classes.item2} ${!isFirstLoad ? classes.loaded : ""}`}>
+        We are Jane Doe's Diner and we serve food and drinks
+      </h2>
+      <div
+        className={`${classes.buttonContainer} ${classes.item3} ${
+          classes.item3
+        } ${!isFirstLoad ? classes.loaded : ""}`}
+      >
         <DinerButton text="VIEW MAIN MENU" onClick={clickHanlder} />
         <DinerButton text="ORDER ONLINE" onClick={clickHanlder} />
       </div>
