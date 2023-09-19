263b141c339fd3ba4ac538f6a176920caba52fce
Taylor Casse <taylorcasse@gmail.com>
2023-09-19 16:24:22 +0200

    Security fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ModalContextProvider } from "./store/ModalContext";
 import SidebarModal from "./modals/SidebarModal";
-import Sidebar from "./components/Sidebar/Sidebar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
