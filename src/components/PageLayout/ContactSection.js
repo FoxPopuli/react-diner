4a210f8e1b26b897c77bf83fa309ac5e22f1d819
Taylor Casse <taylorcasse@gmail.com>
2023-09-23 02:05:17 +0200

    Add ContactSection

--- src/components/PageLayout/ContactSection.js
+++ src/components/PageLayout/ContactSection.js
@@ -1,1 +1,31 @@
+import { Link } from "react-router-dom";
 import classes from "./ContactSection.module.css";
+
+const ContactSection = () => {
+  return (
+    <section className={classes.main}>
+      <div className={`${classes.leftColumn}`}>
+        <div className={`${classes.contact}`}>
+          <h2>Contact</h2>
+          <p className={`${classes.location}`}></p>
+        </div>
+        <div className={`${classes.follow}`}>
+          <h2>Follow Us</h2>
+          <Link to="/">Instagram</Link>
+          <Link to="/">Facebook</Link>
+        </div>
+      </div>
+      <div className={`${classes.rightColumn}`}>
+        <span>T: +27 82 000 0000</span>
+        <span>E: manager@janesdiner.co.za</span>
+        <div className={classes.tradingHours}>
+          <div>
+            <span>Sunday to</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactSection;
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,10 @@
+import ContactSection from "../components/PageLayout/ContactSection";
+
 const ContactPage = () => {
   return (
     <section>
       <h1>Contact</h1>
-      <p>Under Construction!</p>
+      <ContactSection />
     </section>
   );
 };
