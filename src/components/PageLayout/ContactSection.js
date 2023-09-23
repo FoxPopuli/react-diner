5aff73626e1caaf7d16822944b3be98f0931954a
Taylor Casse <taylorcasse@gmail.com>
2023-09-23 18:33:47 +0200

    Add basic styling to ContactSection

--- src/components/PageLayout/ContactSection.js
+++ src/components/PageLayout/ContactSection.js
@@ -5,23 +5,32 @@ const ContactSection = () => {
   return (
     <section className={classes.main}>
       <div className={`${classes.leftColumn}`}>
-        <div className={`${classes.contact}`}>
+        <div className={`${classes.group1}`}>
           <h2>Contact</h2>
           <p className={`${classes.location}`}></p>
         </div>
-        <div className={`${classes.follow}`}>
+        <div className={`${classes.group2}`}>
           <h2>Follow Us</h2>
           <Link to="/">Instagram</Link>
           <Link to="/">Facebook</Link>
         </div>
       </div>
       <div className={`${classes.rightColumn}`}>
-        <span>T: +27 82 000 0000</span>
-        <span>E: manager@janesdiner.co.za</span>
-        <div className={classes.tradingHours}>
-          <div>
-            <span>Sunday to</span>
-          </div>
+        <div className={`${classes.group1}`}>
+          <span>T: +27 82 000 0000</span>
+          <span>E: manager@janesdiner.co.za</span>
+        </div>
+
+        <div className={`${classes.group2}`}>
+          <h2>Trading Hours</h2>
+          <p className={classes.tradingHourGroup}>
+            <span>Sunday to Thursday</span>
+            <span>Friday & Saturday</span>
+          </p>
+          <p className={classes.tradingHourGroup}>
+            <span>11:00AM - 21:00PM</span>
+            <span>11:00AM - 22:00PM</span>
+          </p>
         </div>
       </div>
     </section>
