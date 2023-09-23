da4103d623550d3664f9c13c24d43c7f8fe11231
Taylor Casse <taylorcasse@gmail.com>
2023-09-23 18:46:36 +0200

    Restructure ContactSection

--- src/components/PageLayout/ContactSection.js
+++ src/components/PageLayout/ContactSection.js
@@ -5,27 +5,27 @@ import ContactCard from "../ContactCard/ContactCard";
 const ContactSection = () => {
   return (
     <section className={classes.main}>
-      <div className={`${classes.leftColumn}`}>
-        <div className={`${classes.group1}`}>
+      <div className={`${classes.group1}`}>
+        <div className={classes.leftAlgin}>
           <h2>Contact</h2>
-          <p className={`${classes.location}`}></p>
         </div>
-        <div className={`${classes.group2}`}>
+        <div className={`${classes.rightAlign}`}>
+          <span>T: +27 82 000 0000</span>
+          <span>E: manager@janesdiner.co.za</span>
+        </div>
+      </div>
+      <div className={`${classes.group2}`}>
+        <div className={classes.leftAlign}>
           <h2>Follow Us</h2>
           <Link to="/">Instagram</Link>
           <Link to="/">Facebook</Link>
         </div>
-      </div>
-      <div className={`${classes.rightColumn}`}>
-        <div className={`${classes.group1}`}>
-          <span>T: +27 82 000 0000</span>
-          <span>E: manager@janesdiner.co.za</span>
-        </div>
-
-        <div className={`${classes.group2}`}>
+        <div className={classes.rightAlign}>
           <ContactCard />
         </div>
       </div>
+
+      <div className={`${classes.group2}`}></div>
     </section>
   );
 };
