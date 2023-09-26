cb7e7a4d16ec45649246baa3ed7145e34fc37166
Taylor Casse <taylorcasse@gmail.com>
2023-09-26 16:07:00 +0200

    Change reservations phone number and quote

--- src/components/PageLayout/ReservationsSection.js
+++ src/components/PageLayout/ReservationsSection.js
@@ -19,13 +19,13 @@ const ReservationsSection = () => {
           className={`${classes.leftColumn} ${isLoaded ? classes.loaded : ""}`}
         >
           <h2>
-            "Perfection is a lot of little things done right" - Marco Pierre
-            White
+            "What is the charge? Eating a meal? A succulent Chinese meal?" -
+            Charles Dozsa
           </h2>
           <h3>Reserve by Phone</h3>
           <p>
             To ensure that we can discuss your booking and seating requirements
-            to ensure they are to your liking, please call us on 0877112001
+            to ensure they are to your liking, please call us on 080 000 0000
             between 11:00 am – 21:00 pm, Monday to Sunday.
           </p>
         </div>
