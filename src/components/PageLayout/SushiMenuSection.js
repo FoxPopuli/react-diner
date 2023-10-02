d4ac1ec93c55c5468014aab68923a300e5778224
Taylor Casse <taylorcasse@gmail.com>
2023-10-02 13:59:11 +0200

    Add boilerplate code for SushiMenuSection

---
+++ src/components/PageLayout/SushiMenuSection.js
@@ -0,0 +1,7 @@
+import classes from "./SushiMenuSection.module.css";
+
+const SushiMenuSection = () => {
+  return <section className={classes.main}></section>;
+};
+
+export default SushiMenuSection;
