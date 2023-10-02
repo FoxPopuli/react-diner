8f0307b65fbffaca7d1b7f324bf3ca0fa008d456
Taylor Casse <taylorcasse@gmail.com>
2023-10-02 18:43:39 +0200

    Add animations to construction section

--- src/components/PageLayout/ConstructionSection.js
+++ src/components/PageLayout/ConstructionSection.js
@@ -1,10 +1,21 @@
 import classes from "./ConstructionSection.module.css";
+import { useState, useEffect } from "react";
 
 const ConstructionSection = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
     <section className={classes.main}>
-      <h1>Under Construction</h1>
-      <h2>...For now</h2>
+      <h1 className={`${classes.heading} ${isLoaded ? classes.loaded : ""}`}>
+        Under Construction
+      </h1>
+      <h2 className={`${classes.subHeading} ${isLoaded ? classes.loaded : ""}`}>
+        ...For now
+      </h2>
     </section>
   );
 };
