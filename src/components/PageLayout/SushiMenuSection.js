7f5a38f224366d8d1ff5bff04e95a853ff7a081a
Taylor Casse <taylorcasse@gmail.com>
2023-10-03 13:01:41 +0200

    Add sushi menu data

--- src/components/PageLayout/SushiMenuSection.js
+++ src/components/PageLayout/SushiMenuSection.js
@@ -1,7 +1,11 @@
 import classes from "./SushiMenuSection.module.css";
 
 const SushiMenuSection = () => {
-  return <section className={classes.main}></section>;
+  return (
+    <section className={classes.main}>
+      <h1>Sushi Menu</h1>
+    </section>
+  );
 };
 
 export default SushiMenuSection;
---
+++ src/data/sushiMenuData.js
@@ -0,0 +1,79 @@
+const sushiMenuData = [
+  {
+    title: "NIGIRI",
+    subTitle: "TWO FINGERS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet purus non massa rhoncus consequat. Sed eget aliquam diam, inviverra magna. Proin pulvinar dignissim ornare. Ut tortor eros, suscipitvel diam ut, sodales luctus elit.",
+    items: [
+      {
+        title: "NIGIRI DISH 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        price: 120,
+      },
+      {
+        title: "NIGIRI DISH 2",
+        description:
+          "Nullam imperdiet purus non massa rhoncus consequat. Sed eget aliquam diam, inviverra magna. Proin pulvinar dignissim ornare. Ut tortor eros, suscipitvel diam ut, sodales luctus elit.",
+        price: 150,
+      },
+      {
+        title: "NIGIRI DISH 3",
+        description:
+          "Ut tortor eros, suscipit vel diam ut, sodales luctus elit. Ut pellentesque purus bibendum, cursus risus vel, dapibus diam. Integer iaculis dictum est, maximus finibus.",
+        price: 150,
+      },
+    ],
+  },
+  {
+    title: "URAMAKI",
+    subTitle: "INSIDE OUT ROLL",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet purus non massa rhoncus consequat. Sed eget aliquam diam, inviverra magna. Proin pulvinar dignissim ornare. Ut tortor eros, suscipitvel diam ut, sodales luctus elit.",
+    items: [
+      {
+        title: "URAMAKI 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        price: 120,
+      },
+      {
+        title: "URAMAKI 2",
+        description:
+          "Nullam imperdiet purus non massa rhoncus consequat. Sed eget aliquam diam, inviverra magna. Proin pulvinar dignissim ornare. Ut tortor eros, suscipitvel diam ut, sodales luctus elit.",
+        price: 150,
+      },
+      {
+        title: "URAMAKI 3",
+        description:
+          "Ut tortor eros, suscipit vel diam ut, sodales luctus elit. Ut pellentesque purus bibendum, cursus risus vel, dapibus diam. Integer iaculis dictum est, maximus finibus.",
+        price: 150,
+      },
+    ],
+  },
+  {
+    title: "RAMEN",
+    subTitle: "PULLED NOODLES",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet purus non massa rhoncus consequat. Sed eget aliquam diam, inviverra magna. Proin pulvinar dignissim ornare. Ut tortor eros, suscipitvel diam ut, sodales luctus elit.",
+    items: [
+      {
+        title: "RAMEN 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        price: 120,
+      },
+      {
+        title: "RAMEN 2",
+        description:
+          "Nullam imperdiet purus non massa rhoncus consequat. Sed eget aliquam diam, inviverra magna. Proin pulvinar dignissim ornare. Ut tortor eros, suscipitvel diam ut, sodales luctus elit.",
+        price: 150,
+      },
+      {
+        title: "RAMEN 3",
+        description:
+          "Ut tortor eros, suscipit vel diam ut, sodales luctus elit. Ut pellentesque purus bibendum, cursus risus vel, dapibus diam. Integer iaculis dictum est, maximus finibus.",
+        price: 150,
+      },
+    ],
+  },
+];
+
+export default sushiMenuData;
